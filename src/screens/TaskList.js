592ef8247ac311dbf8504136691db4820132dcbd
Nathan de oliveria <nosousa.2000@gmail.com>
2021-02-26 14:21:55 -0300

    task list component 1

---
+++ src/screens/TaskList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, SafeAreaView } from 'react-native'
+export default class TaskList extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>TaskList</Text>
+                <Text>TaskList</Text>
+                <Text>TaskList</Text>
+                <Text>TaskList</Text>
+                <Text>TaskList</Text>
+            </SafeAreaView>
+        )
+    }
+}
