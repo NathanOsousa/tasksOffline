8eaf0d0c0785534f6cd2b4ddb6f46d7876d6e38e
Nathan de oliveria <nosousa.2000@gmail.com>
2021-02-26 15:23:11 -0300

    styles in task component

--- src/components/Task/style.js
+++ src/components/Task/style.js
@@ -5,30 +5,10 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
     container: {
-        flexGrow: 1
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'red',
+        alignItems: 'center',
+        paddingVertical: 10
     },
-    backGround: {
-        flexGrow: 3
-    },
-    taskList: {
-        flexGrow: 7,
-    },
-    titleBar: {
-        flex: 1,
-        justifyContent: 'flex-end',
-    },
-    title: {
-        fontFamily: globalStyles.fontFamily,
-        color: globalStyles.colors.secondary,
-        fontSize: 50,
-        marginLeft: 20,
-        marginBottom: 20,
-    },
-    subtitle: {
-        fontFamily: globalStyles.fontFamily,
-        color: globalStyles.colors.secondary,
-        fontSize: 20,
-        marginLeft: 20,
-        marginBottom: 20,
-    }
 })
--- src/components/Task/task.js
+++ src/components/Task/task.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { Text, View, ImageBackground } from 'react-native'
 import style from './style'
-import todayImage from '../../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt'
 
-const today = moment().local('pt-br').format('ddd, D [de] MMMM');
+
 export default props => {
     return (
         <View style={style.container}>
