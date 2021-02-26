845bc7b6a5bf7b642466f35cc42dd327f4db19f5
Nathan de oliveria <nosousa.2000@gmail.com>
2021-02-26 11:41:59 -0300

    implemented lind and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+
+  "extends": [
+    "plugin:prettier/recommended"
+  ],
+
+  "plugins": ["prettier"],
+
+  "rules": {
+    "prettier/prettier": "error"
+  }
+
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false
 };
