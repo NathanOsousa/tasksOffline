14fbe12a973cf497a80d4639ba8a6551065cd5a6
nathan de oliveira <nosousa.2000@gmail.com>
2021-04-15 14:57:14 -0300

    lint

--- src/components/Task/task.js
+++ src/components/Task/task.js
@@ -1,46 +1,43 @@
-import React from 'react'
-import { Text, View, ImageBackground, TouchableWithoutFeedback } from 'react-native'
-import style from './style'
-import Icon from 'react-native-vector-icons/FontAwesome'
-import moment from 'moment'
-import 'moment/locale/pt-br'
-
-
+import React from 'react';
+import {
+  Text,
+  View,
+  ImageBackground,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import style from './style';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 export default props => {
+  const isDoneStyle =
+    props.doneAt != null ? {textDecorationLine: 'line-through'} : {};
 
-
-    const isDoneStyle = props.doneAt != null ? { textDecorationLine: 'line-through' } : {}
-
-    const getCheckView = (doneAt) => {
-        if (doneAt != null) {
-            return (
-                <View style={style.done}>
-                    <Icon name="check" size={20} color="#FFF" />
-                </View>
-            )
-        } else {
-            return (
-                <View style={style.pendding} />
-            )
-        }
-    }
-
-    const date = props.doneAt ? props.doneAt : props.estimatedAt
-    const formatedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM')
-
-    return (
-        <View style={style.container}>
-            <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
-                <View style={style.checkContainer}>
-                    {getCheckView(props.doneAt)}
-                </View>
-            </TouchableWithoutFeedback>
-            <View>
-                <Text style={[style.desc, isDoneStyle]}> {props.desc} </Text>
-                <Text style={style.date}> {formatedDate}</Text>
-            </View>
+  const getCheckView = doneAt => {
+    if (doneAt != null) {
+      return (
+        <View style={style.done}>
+          <Icon name="check" size={20} color="#FFF" />
         </View>
-    )
+      );
+    } else {
+      return <View style={style.pendding} />;
+    }
+  };
 
-}
+  const date = props.doneAt ? props.doneAt : props.estimatedAt;
+  const formatedDate = moment(date).locale('pt-br').format('ddd, D [de] MMMM');
+
+  return (
+    <View style={style.container}>
+      <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+        <View style={style.checkContainer}>{getCheckView(props.doneAt)}</View>
+      </TouchableWithoutFeedback>
+      <View>
+        <Text style={[style.desc, isDoneStyle]}> {props.desc} </Text>
+        <Text style={style.date}> {formatedDate}</Text>
+      </View>
+    </View>
+  );
+};
